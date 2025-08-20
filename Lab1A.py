3821fa71620993084ff0fc99ee6db32cdde46220
Andrew Gilley <andrewjamesgilley@gmail.com>
2025-08-20 11:24:21 -0400

    File upload

---
+++ Lab1A.py
@@ -0,0 +1,21 @@
+# Class: CSE 1321L
+# Section: W01
+# Term: Fall Semester 2025 
+# Instructor: Venkata Palagundla 
+# Name: Andrew Gilley
+# Lab: #1
+
+# Program Lab1A.py
+
+# Demonstrate the difference between print with and without a newline. 
+# Prints two lines of output representing a rocket countdown.
+
+print("Three...", end = " ")
+print("Two...", end = " ")
+print("One...", end = " ")
+
+print ("Zero...", end = " ")
+print ("Liftoff!")
+
+# Appears on first line.
+print("Houston, we have a problem.")
---
+++ Lab1B.py
@@ -0,0 +1,14 @@
+# Class: CSE 1321L
+# Section: W01
+# Term: Fall Semester 2025 
+# Instructor: Venkata Palagundla 
+# Name: Andrew Gilley
+# Lab: #1
+
+# Program Lab1B.py 
+
+# Demonstrate reading a string from the user.   
+# Reads a character string from the user and prints it.  
+
+message = input ("Enter a line of text: ")
+print ("You wrote '" + message + "'")
