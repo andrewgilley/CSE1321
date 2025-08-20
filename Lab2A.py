812a6f3b605ad36a8ae1b6ed5d3873ea380e8bd1
Andrew Gilley <andrewjamesgilley@gmail.com>
2025-08-20 13:34:38 -0400

    Lab 2 update

---
+++ Lab2A.py
@@ -0,0 +1,16 @@
+# Class: CSE 1321L
+# Section: W01
+# Term: Fall Semester 2025 
+# Instructor: Venkata Palagundla 
+# Name: Andrew Gilley
+# Lab: #2
+
+name_1 = input("Enter a name: ")
+name_2 = input("Enter another name: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+
+print("Once upon a time, there was a person named ", end = name_1 + " ") 
+print("who had a child named ", end = name_2 + ". ")
+print("This child would ", end = verb + " " + adverb + " ")
+print("while singing to strangers.")
