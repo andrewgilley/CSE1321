7633cfa3cafa1bec87f61ad614166fc5bc999fb1
Andrew Gilley <andrewjamesgilley@gmail.com>
2025-08-20 14:42:00 -0400

    Project update.

---
+++ Assignments/Assignment1/Assignment1A.py
@@ -0,0 +1,13 @@
+# Class: CSE 1321L
+# Section: W01
+# Term: Fall Semester 2025 
+# Instructor: Venkata Palagundla 
+# Name: Andrew Gilley
+# Assignment: #1
+
+name = input("Enter your name: ") 
+item = input("Enter the item you purchased: ") 
+quantity = input("Enter the quantity of the item: ") 
+
+print("Thank you, ", end = name + "! ")
+print("You ordered ", end = quantity + " " + item + "(s).")
